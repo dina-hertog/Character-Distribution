a839203fd9342242896eca88a8b020c3bfb641a6
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-10-27 14:30:16 -0400

    Updated from Brython Server: 10/27/2015 2:30:16 PM

--- distribution.py
+++ distribution.py
@@ -36,8 +36,8 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-text = input("Please enter a string of text (the bigger the better):")
-print("The distribution of characters in " + text + " is:")
+text = input("Please enter a string of text (the bigger the better): ")
+print("The distribution of characters in " + text + " is: ")
 text = text.lower()
 length = len(text)
 letter = 0
